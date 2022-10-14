e1be41b5733e126943979756e31d1e8e1674ef71
acharsaa <acharsaa@W5CG81966SH>
2022-10-14 16:03:52 +0530

    Commit

---
+++ mtom-jax-ws-consumer/src/com/training/Application.java
@@ -0,0 +1,56 @@
+package com.training;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class Application {
+    public static void uploadImage() {
+        ImageProcessing service = new ImageProcessingImplService().getImageProcessingImplPort();
+        try {
+            BufferedImage buffImage = ImageIO.read(new File("download.jpg"));
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            ImageIO.write(buffImage, "png", stream);
+            byte[] bytes=stream.toByteArray();
+            System.out.println(service.sendImage(bytes));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+
+   public static void downloadImage() {
+           ImageProcessing service = new ImageProcessingImplService().getImageProcessingImplPort();
+            String imageName = "ceo.png";
+            byte[] bytes = service.getImage(imageName);
+            InputStream stream = new ByteArrayInputStream(bytes);
+            try {
+                BufferedImage buffImage = ImageIO.read(stream);
+                ImageIO.write(buffImage, "png", new File(imageName));
+                
+                System.out.println("Image Saved");
+                
+                JFrame frame = new JFrame("Show Image");
+                frame.setSize(300,250);
+                
+                JLabel label = new JLabel(new ImageIcon(buffImage));
+                frame.add(label);
+                frame.setVisible(true);
+            }
+            catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+      public static void main(String[] args) {
+        //uploadImage();
+        downloadImage();
+    }
+}
